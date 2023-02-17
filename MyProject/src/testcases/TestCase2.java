f73743ad6ef76a5fcacf44740899d9523234cccc
MANISH-droid-cloud <119052393+MANISH-droid-cloud@users.noreply.github.com>
2023-02-17 11:20:16 +0530

    Testcase 2 updated

--- MyProject/src/testcases/TestCase2.java
+++ MyProject/src/testcases/TestCase2.java
@@ -4,7 +4,9 @@ public class TestCase2 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("This is Testcase 2");
+		System.out.println("This is Testcase 2 updated.....");
+		
+		
 
 	}
 
